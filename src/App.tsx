2aa8ca5dc5cf02c652c2dbd64018a6e4e42c6ccb
Bassam Husain <61985779+bassamanator@users.noreply.github.com>
2022-12-04 21:14:05 -0500

    Adding vitest and re.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState(4);
 
 	return (
 		<div className='mx-auto w-96'>
-			<h1 className={'text-lg text-6xl'}>Reacdle</h1>
+			<h1 className={' text-6xl'}>Reacdle</h1>
+			<h2>count IS: {count}</h2>
+
+			<button onClick={() => setCount((p) => p + 1)}>increment</button>
 		</div>
 	);
 }
